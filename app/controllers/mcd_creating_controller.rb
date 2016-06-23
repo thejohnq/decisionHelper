0d9e88c8b0a16144d41a5c8ae0d6b83ced076bc4
Presiyan Peev <pressijan@gmail.com>
2016-06-23 20:38:23 +0000

    Create Array in controller

--- app/controllers/mcd_creating_controller.rb
+++ app/controllers/mcd_creating_controller.rb
@@ -1,13 +1,23 @@
 class McdCreatingController < ApplicationController
+  
+  @@arrCriteriaGlobal = Array.new();
+  
   def createProblem
   end
 
   def createCriteria
+    @arrCriteria = @@arrCriteriaGlobal;
   end
 
   def createAlternatives
+    
   end
 
   def overview
   end
+  
+  def addCritToArr
+    @arrCriteriaGlobal << 6
+  end
+  
 end
